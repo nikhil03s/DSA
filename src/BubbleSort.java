d1c0259558073b7514456abd16acb642e85be303
Nikhil <nikhilshirke1712@gmail.com>
2025-08-28 07:57:22 +0530

    Sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,32 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int []a={5,6,13,65,26,48,31};
+        int temp=0;
+        System.out.println("Before Sort");
+        for(int n:a)
+        {
+            System.out.print(n+" ");
+        }
+
+        for(int i=0;i<a.length-1;i++)
+        {
+            for(int j=0;j<a.length-1;j++)
+            {
+                if(a[j]>a[j+1])
+                {
+                   temp=a[j];
+                   a[j]=a[j+1];
+                   a[j+1]=temp;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("After Sort");
+        for(int n:a)
+        {
+            System.out.print(n+" ");
+        }
+
+
+    }
+}
