2d3d07d2c0fe4e8a6091915c6e830183190b3ac5
Nikhil <nikhilshirke1712@gmail.com>
2025-12-07 13:42:15 +0530

    DAY II

--- src/StarPatterns.java
+++ src/StarPatterns.java
@@ -66,7 +66,7 @@ public class StarPatterns {
         {
             for (int j=1;j<=i;j++)
                 if(j==1 || j==n)
-                    System.out.print(j);
+                    System.out.printgi(j);
                 else
                     System.out.print(j*2);
             System.out.println();
